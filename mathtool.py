a2d5e13ef0441b949639004a3c39642b02433a0b
sephulcore <zoriyilya@gmail.com>
2026-09-08 09:15:50 +0300

    err

--- mathtool.py
+++ mathtool.py
@@ -3,3 +3,6 @@ args = sys.argv[1:]
 if len(args) == 0 or args[0] == '--help':
     print('mathtool - решение уравнений вида A*x^2 + B*x + C = 0')
     sys.exit(0)
+if args[0] != 'solve':
+    print(f'Ошибка:неизвестная команда', file=sys.stderr)
+    sys.exit(1)
