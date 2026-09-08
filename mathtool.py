72d6a67cae5dda82f364ede94ea4b7f88931124e
sephulcore <zoriyilya@gmail.com>
2026-09-08 20:59:43 +0300

    proverka

--- mathtool.py
+++ mathtool.py
@@ -1,11 +1,14 @@
 import sys
+import math
 args = sys.argv[1:]
-if len(args) == 0 or args[0] == '--help':  # Вывод справки
+if len(args) == 0 or args[0] == '--help':
     print('mathtool - решение уравнений вида A*x^2 + B*x + C = 0')
     sys.exit(0)
-if args[0] != 'solve':  # Неизвестная комана(не solve)
+
+if args[0] != 'solve':  
     print(f'Ошибка:неизвестная команда', file=sys.stderr)
     sys.exit(1)
+
 if len(args) == 1:
     a_s = input('Введите A:')
     b_s = input('Введите B:')
@@ -17,3 +20,14 @@ elif len(args) == 7 and args[1] == '-a' and args[3] == '-b' and args[5] == '-c':
 else:
     print('Ошибка: неверный набор параметров', file=sys.stderr)
     sys.exit(1)
+
+try:
+        a = int(a_s)
+        b = int(b_s)
+        c = int(c_s)
+except ValueError:
+    print('Ошибка: коэффициент не является целым числом', file=sys.stderr)
+    sys.exit(1)
+if abs(a) > 10_000 or abs(b) > 10_000 or abs(c) > 10_000:
+     print('Ошибка: значение вне допустимого диапазона', file=sys.stderr)
+     sys.exit(1)
